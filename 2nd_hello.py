b0771fea1bb0e9aa84518829fadac37d43438313
benzenez1 <benzene457585@gmil.com>
2020-07-26 00:29:01 +0900

    add same file with hello.py

---
+++ 2nd_hello.py
@@ -0,0 +1,10 @@
+
+def main():
+    print("Same behavior with first_test")
+    str = "Hello world"
+    print(str+" (as variable output)")
+    print("Hello world (directly output)")
+
+
+if __name__ == "__main__":
+    main()
